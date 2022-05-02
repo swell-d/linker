9a84bfa4c73bddeb3ad68683f6a8d1b5f1876602
Dmitry Rude <3952800@gmail.com>
2022-05-02 11:04:36 +0200

    links table prettify

--- main/models.py
+++ main/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Link(models.Model):
     alias = models.CharField(max_length=256, null=False, blank=False, verbose_name='Alias', unique=True)
     link = models.URLField(null=False, blank=False, verbose_name='Link')
+
+    def __str__(self):
+        return f'{self.alias} :: {self.link}'
