23a166ab8bea886ef234d5fb82baa6a402d8b372
swell-d <3952800@gmail.com>
2023-06-16 18:05:31 +0200

    linker24.com

--- linker/settings.py
+++ linker/settings.py
@@ -112,8 +112,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# WHITENOISE_MANIFEST_STRICT = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
